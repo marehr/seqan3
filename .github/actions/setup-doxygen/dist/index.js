2366bf5c287762a2f805f45fd03899050f20cb57
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2020-02-14 11:30:53 +0100

    now use only setup-doxygen

--- .github/actions/setup-doxygen/dist/index.js
+++ .github/actions/setup-doxygen/dist/index.js
@@ -12366,20 +12366,26 @@ async function run() {
     try {
         // `who-to-greet` input defined in action metadata file
         const nameToGreet = core.getInput('who-to-greet');
+        let version = core.getInput('version');
+        if (!version) {
+          version = core.getInput('doxygen-version');
+        }
         console.log(`Hello ${nameToGreet}!`);
-        const time = (new Date()).toTimeString();
+        console.log(`Hello ${version}!`);
+        // const time = (new Date()).toTimeString();
         core.setOutput("time", time);
         const doxygen_version = "1.8.17";
 
         const doxygen_archive = await tool_cache.downloadTool(
-            'https://sourceforge.net/projects/doxygen/files/'+
-            'rel-' + doxygen_version + '/doxygen-' + doxygen_version + '.linux.bin.tar.gz');
+            `https://sourceforge.net/projects/doxygen/files/rel-${doxygen_version}/doxygen-${doxygen_version}.linux.bin.tar.gz`);
 
         const doxygen_bin_folder = await tool_cache.extractTar(
-            doxygen_archive, '/tmp/doxygen-' + doxygen_version + '/bin');
+            doxygen_archive, `/tmp/doxygen-${doxygen_version}`);
 
-        const cached_path = await tool_cache.cacheDir(doxygen_bin_folder, 'doxygen', doxygen_version);
-        core.addPath(cached_path);
+        const doxygen_cache_path = await tool_cache.cacheDir(doxygen_bin_folder, 'doxygen', doxygen_version);
+
+        // add this path to $PATH for subsequent setos
+        core.addPath(doxygen_cache_path + '/bin');
 
     } catch (error) {
         core.setFailed(error.message);
