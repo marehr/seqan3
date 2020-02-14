dc75d85ee10510d3a38b29ac4093084f3e8c1b50
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2020-02-14 14:06:13 +0100

    more standard conform

--- .github/actions/setup-doxygen/src/setup-doxygen.js
+++ .github/actions/setup-doxygen/src/setup-doxygen.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const tool_cache = require('@actions/tool-cache');
 
 async function download_node(version) {
