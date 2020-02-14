9c69c013ba199edc16c8f4e0ff22c8b1f9e0266e
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2020-02-14 13:03:11 +0100

    upps

--- .github/actions/setup-doxygen/dist/index.js
+++ .github/actions/setup-doxygen/dist/index.js
@@ -12367,33 +12367,33 @@ async function download_node(version) {
 
     // there is a cached version of doxygen, use it
     if (cached_tool_path) {
-        console.log(`Found cached doxygen ${doxygen_version}.`)
+        console.log(`Found cached doxygen ${version}.`)
         return cached_tool_path;
     }
 
     // acquire doxygen
-    console.log(`Attempt to download doxygen ${doxygen_version}.`);
+    console.log(`Attempt to download doxygen ${version}.`);
 
     const doxygen_archive = await tool_cache.downloadTool(
-        `https://sourceforge.net/projects/doxygen/files/rel-${doxygen_version}/doxygen-${doxygen_version}.linux.bin.tar.gz`);
+        `https://sourceforge.net/projects/doxygen/files/rel-${version}/doxygen-${version}.linux.bin.tar.gz`);
 
     const doxygen_extracted_folder = await tool_cache.extractTar(
         doxygen_archive, `/tmp`);
 
     // i.e. will be /opt/hostedtoolcache/doxygen/1.8.17/x64
     return await tool_cache.cacheDir(
-        `${doxygen_extracted_folder}/doxygen-${doxygen_version}`,
-        'doxygen', doxygen_version);
+        `${doxygen_extracted_folder}/doxygen-${version}`,
+        'doxygen', version);
 }
 
 async function run() {
     try {
-        let doxygen_version = core.getInput('version');
+        let version = core.getInput('version');
         if (!doxygen_version) {
-          doxygen_version = core.getInput('doxygen-version');
+            version = core.getInput('doxygen-version');
         }
-        console.log(`Requested doxygen ${doxygen_version}.`);
-        const tool_path = await download_node(doxygen_version);
+        console.log(`Requested doxygen ${version}.`);
+        const tool_path = await download_node(version);
 
         // add this path to $PATH for subsequent setos
         core.addPath(tool_path + '/bin');
