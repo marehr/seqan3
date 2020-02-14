012a918ee4f3a0b9ffd8dc92d1b59abc5ceef142
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2020-02-14 11:45:39 +0100

    now use only setup-doxygen 3rd try

--- .github/actions/setup-doxygen/src/setup-doxygen.js
+++ .github/actions/setup-doxygen/src/setup-doxygen.js
@@ -6,23 +6,29 @@ async function run() {
     try {
         // `who-to-greet` input defined in action metadata file
         const nameToGreet = core.getInput('who-to-greet');
-        let version = core.getInput('version');
-        if (!version) {
-          version = core.getInput('doxygen-version');
+        let doxygen_version = core.getInput('version');
+        if (!doxygen_version) {
+          doxygen_version = core.getInput('doxygen-version');
         }
         console.log(`Hello ${nameToGreet}!`);
-        console.log(`Hello ${version}!`);
+        console.log(`Hello ${doxygen_version}!`);
         // const time = (new Date()).toTimeString();
         // core.setOutput("time", time);
-        const doxygen_version = "1.8.17";
+        // const doxygen_version = "1.8.17";
 
         const doxygen_archive = await tool_cache.downloadTool(
             `https://sourceforge.net/projects/doxygen/files/rel-${doxygen_version}/doxygen-${doxygen_version}.linux.bin.tar.gz`);
+        console.log(`doxygen_archive: ${doxygen_archive}`);
 
-        const doxygen_bin_folder = await tool_cache.extractTar(
-            doxygen_archive, `/tmp/doxygen-${doxygen_version}`);
+        const doxygen_extracted_folder = await tool_cache.extractTar(
+            doxygen_archive, `/tmp`);
+        console.log(`doxygen_extracted_folder: ${doxygen_extracted_folder}`);
 
-        const doxygen_cache_path = await tool_cache.cacheDir(doxygen_bin_folder, 'doxygen', doxygen_version);
+        const doxygen_folder = `${doxygen_extracted_folder}/doxygen-${doxygen_version}`;
+        console.log(`doxygen_extracted_folder: ${doxygen_extracted_folder}`);
+
+        const doxygen_cache_path = await tool_cache.cacheDir(doxygen_folder, 'doxygen', doxygen_version);
+        console.log(`doxygen_cache_path: ${doxygen_cache_path}`);
 
         // add this path to $PATH for subsequent setos
         core.addPath(doxygen_cache_path + '/bin');
